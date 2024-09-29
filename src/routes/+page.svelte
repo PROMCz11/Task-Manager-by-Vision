d8129cb44d36fbe905ef876049219df4dca55799
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-09-29 13:51:04 +0300

    Importing Task Manager into Sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div>
+    <!-- To be deleted -->
+    <section style="position: absolute; top: 50%; left: 50%; translate: -50% -50%;">
+        <h2>Task Manager testing page</h2>
+        <p style="margin-top: .7rem; margin-bottom: 2rem;">An ongoing project by <span style="color: black; background-color: white; font-weight: 800; font-size: 1.1rem;">Vision</span></p>
+        <p>Please follow one of the links below,</p>
+        <p>Enjoy testing.</p>
+    </section>
+
+    <section style="position: absolute; display: flex; flex-direction: column;
+    gap: 1rem; inset: auto 1rem 2rem 1rem;">
+        <a style="color: lime;" href="sign-up.html">Sign up Page</a>
+        <a style="color: lime;" href="log-in.html">Log in Page</a>
+    </section>
+</div>
+
+<style>
+    div {
+        color: white;
+        background-color: black;
+        font-family: monospace; 
+        text-align: center;
+        min-height: 100vh;
+        min-height: 100svh;
+    }
+</style>
